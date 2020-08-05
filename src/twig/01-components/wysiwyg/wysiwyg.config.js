4bb9553da53c3a8ec3a08c367449e19fec2753fd
Michael Scherr <mscherr@fastspot.com>
2020-08-05 11:47:30 -0400

    adds more fonts

--- src/twig/01-components/wysiwyg/wysiwyg.config.js
+++ src/twig/01-components/wysiwyg/wysiwyg.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	label: 'WYSIWYG',
 	context: {
 		content:
 			'<h2>Nullam dictum felis eu pede mollis pretium</h2><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis <a href="#">natoque penatibus</a> et magnis dis parturient montes, nascetur ridiculus mus.</p><p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>'
