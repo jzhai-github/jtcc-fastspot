e3b554e81f078655c79021d0b4489695d4e164ef
Bryan <bryan.stoner@bytelion.com>
2020-08-30 15:56:12 -0400

    student activities development

--- src/twig/05-partials/contact-details/contact-details.config.js
+++ src/twig/05-partials/contact-details/contact-details.config.js
@@ -1,24 +1,64 @@
 module.exports = {
-	context: {
-		item: {
-			name: 'Name',
-			title: 'Title',
-			image: '1',
-			phone_numbers: [
-				{
-					label: 'mobile',
-					type: 'telephone',
-					number: '(410) 555-1234'
+	variants: [
+		{
+			name: 'chester',
+			context: {
+				item: {
+					title: "Chester Campus",
+					phone_numbers: [
+						{
+							label: 'mobile',
+							type: 'telephone',
+							number: '804-796-4000'
+						}
+					],
+					email: 'studentactivities@jtcc.edu',
+					location: '13101 Jefferson Davis Highway<br>Chester, Virginia 23831-5316',
+					office_hours: [
+						{
+							label: "Monday – Friday",
+							hours: "9:00 am – 7:00 pm"
+						},
+						{
+							label: "Saturday",
+							hours: "11:00 am – 2:00 am"
+						}
+					],
+					social_links: [
+						{
+							title: 'Facebook',
+							url: '#'
+						},
+						{
+							title: 'Twitter',
+							url: '#'
+						}
+					]
 				}
-			],
-			email: 'name@university.edu',
-			location: 'Appleseed Library<br>Room 214',
-			social_links: [
-				{
-					title: 'Linkedin',
-					url: '#'
+			}
+		},
+		{
+			name: 'midlothian',
+			context: {
+				item: {
+					title: "Midlothian Campus",
+					phone_numbers: [
+						{
+							label: 'mobile',
+							type: 'telephone',
+							number: '804-796-4000'
+						}
+					],
+					email: 'studentactivities@jtcc.edu',
+					location: 'T Building, Room 100',
+					office_hours: [
+						{
+							label: "Monday – Friday",
+							hours: "9:00 am – 7:00 pm"
+						}
+					]
 				}
-			]
+			}
 		}
-	}
+	]
 };
