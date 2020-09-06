8cc00cdb68cca8bc44838b8970a3af540cab2a9f
Bryan <bryan.stoner@bytelion.com>
2020-09-06 16:07:23 -0400

    homepage spotlight and stories

---
+++ src/twig/01-components/spotlight/spotlight.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	context: {
+		image: "9",
+		title: {
+			primary: "A Success Story",
+			secondary: "For Every Student"
+		},
+		caption: "At Tyler, we are invested in student success and focused on equipping our graduates with workforce-ready skills. <strong>Will you be next up?</strong>"
+	}
+}
