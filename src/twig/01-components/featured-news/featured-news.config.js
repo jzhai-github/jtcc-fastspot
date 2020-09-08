684028d31f19fd6f30c0d0f8054569e9d2b51d94
Bryan <bryan.stoner@bytelion.com>
2020-09-08 18:17:44 -0400

    featured news development

---
+++ src/twig/01-components/featured-news/featured-news.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	context: {
+		breadcrumb: false,
+		title: "Happening Here",
+		discovery: true,
+		contentTitle: "The Latest News",
+		articles: [
+			{
+				date: "April 21, 2020",
+				title: "State Board for Community Colleges to Consider 2020-21 Fees at May Meeting"
+			},
+			{
+				date: "April 15, 2020",
+				title: "JTCC adds new Maymester Session to Meet the Needs of Students Impacted by the Stay at Home Order"
+			},
+			{
+				date: "March 09, 2020",
+				title: "Workshops and Activities Designed to Help you Succeed"
+			}
+		]
+	}
+}
---
+++ src/twig/01-components/story/story.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	context: {
+		item: {
+			theme: "peach",
+			wideImage: "12",
+			label: "Featured News",
+			title: "Getting Ready for a Successful Spring Semester",
+			description: "It’s a new year, and a new semester is about to begin. While this is an exciting time, it can also be a hectic time for students. Here are a few tips to help you get ready for spring.",
+			link: "Read The Story"
+		}
+	}
+}
