bf1d91aac7f4de3b03c9a663db7f4e210d1878ad
Bryan <bryan.stoner@bytelion.com>
2020-09-13 12:23:47 -0400

    collage development

---
+++ src/twig/01-components/collage/collage.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	context: {
+		title: "In the Classroom",
+		items: [
+			{
+				image: "2",
+				caption: "In her favorite class, Language Arts for Young Children, she practiced reading a children's book to her classmates, enunciating carefully and adding character voices as if she were reading to children."
+			},
+			{
+				image: "3",
+				caption: "Madeline Chase teaching in her cozy corner reading. nook."
+			},
+			{
+				image: "6",
+				caption: "Madeline Chase teaching in her cozy corner reading. nook."
+			},
+			{
+				image: "5",
+				caption: ""
+			}
+		]
+	}
+}
