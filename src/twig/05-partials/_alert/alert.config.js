ddeebc139ff514b6d18b39e327328b1c57075701
Michael Scherr <mscherr@fastspot.com>
2020-09-23 16:38:17 -0400

    reverts alert title change

--- src/twig/05-partials/_alert/alert.config.js
+++ src/twig/05-partials/_alert/alert.config.js
@@ -1,8 +1,7 @@
 module.exports = {
 	context: {
 		published: '2019-10-18 14:52:4',
-		title:
-			'COVID-19 Updates and Closures some more content here and some more',
+		title: 'COVID-19 Updates and Closures',
 		description:
 			'<p>Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. <a href>Learn more.</a></p>'
 	}
