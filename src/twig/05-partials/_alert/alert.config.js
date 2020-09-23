9038018eaf510d3ec4ce7d9312782846456bf45a
Michael Scherr <mscherr@fastspot.com>
2020-09-23 16:06:04 -0400

    works on qc edits

--- src/twig/05-partials/_alert/alert.config.js
+++ src/twig/05-partials/_alert/alert.config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	context: {
 		published: '2019-10-18 14:52:4',
-		title: 'COVID-19 Updates and Closures',
-		description: '<p>Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. <a href>Learn more.</a></p>'
+		title:
+			'COVID-19 Updates and Closures some more content here and some more',
+		description:
+			'<p>Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. <a href>Learn more.</a></p>'
 	}
 };
