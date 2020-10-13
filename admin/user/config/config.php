f2252d1a7b45e5f2a255430b0e63823a75c9ee56
jhorning-tyler <jhorning@jtcc.edu>
2020-10-13 13:26:10 -0400

    Fixed config file

--- admin/user/config/config.php
+++ admin/user/config/config.php
@@ -1,20 +1,23 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['multiple_sites_enabled'] = 'n';
-$config['show_ee_news'] = 'n';
-// Most code copied from:
-// https://u.expressionengine.com/article/creating-a-multi-environment-config-in-expressionengine-5
-$config['save_tmpl_files'] = 'y';
+$config['is_system_on'] = 'y';
+$config['cache_driver'] = 'file';
+$config['doc_url'] = 'http://expressionengine.com/user_guide/';
+$config['index_page'] = '';
+$config['app_version'] = "X.X.X"; // Fill version number here
 
-$protocol           = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "https://" : "http://";
-$base_url           = $protocol . $_SERVER['HTTP_HOST'];
-$base_path          = $_SERVER['DOCUMENT_ROOT'];
-$system_folder      = "admin";
+$system_folder                  = "system";
 
-$images_folder      = "images";
-$images_path        = $base_path . "/" . $images_folder;
-$images_url         = $base_url . "/" . $images_folder;
+$protocol                       = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+$base_url                       = $protocol . $_SERVER['HTTP_HOST'];
+$base_path                      = $_SERVER['DOCUMENT_ROOT'];
 
+$images_folder                  = "images";
+$images_path                    = $base_path . "/" . $images_folder;
+$images_url                     = $base_url . "/" . $images_folder;
+
+
+$config['install_lock']         = "";
 $config['cp_url']               = $base_url.'/'.$system_folder.'/index.php';
 $config['doc_url']              = 'http://expressionengine.com/user_guide/';
 $config['is_system_on']         = 'y';
@@ -23,45 +26,26 @@ $config['site_label']           = 'Site Label';
 $config['site_name']            = 'Site Name';
 $config['cookie_prefix']        = '';
 
-$config['index_page']           = "index.php";
+$config['profile_trigger']      = substr(md5(microtime()),rand(0,26),8);
+
+$config['disable_all_tracking'] = 'y'; # y/n
+
+$config['index_page']           = "";
 $config['base_url']             = $base_url . "/";
 $config['site_url']             = $config['base_url'];
 $config['cp_url']               = $config['base_url'].$system_folder."/index.php";
 $config['theme_folder_path']    = $base_path . "/themes/";
 $config['theme_folder_url']     = $base_url . "/themes/";
 $config['cp_theme']             = "default";
-$config['emoticon_path']        = $images_url . "/smileys/";
-$config['captcha_path']         = $images_path . "/captchas/";
-$config['captcha_url']          = $images_url . "/captchas/";
-$config['avatar_path']          = $images_path . "/avatars/";
-$config['avatar_url']           = $images_url . "/avatars/";
-$config['photo_path']           = $images_path . "/member_photos/";
-$config['photo_url']            = $images_url . "/member_photos/";
-$config['sig_img_path']         = $images_path . "/signature_attachments/";
-$config['sig_img_url']          = $images_url . "/signature_attachments/";
-$config['prv_msg_upload_path']  = $images_path . "/pm_attachments/";
 
-$config['index_page'] = 'index.php';
-$config['save_tmpl_files'] = 'y';
-// ExpressionEngine Config Items
-// Find more configs and overrides at
-// https://docs.expressionengine.com/latest/general/system_configuration_overrides.html
 
-$config['app_version'] = '5.3.2';
-$config['encryption_key'] = '78e307bb286224bd336e71417c800997eb9612a5';
-$config['session_crypt_key'] = 'f4b860726dbfee5bb8dddda232142261abbb8550';
-// $config['database'] = array(
-// 	'expressionengine' => array(
-// 		'hostname' => 'localhost',
-// 		'database' => 'jtcc_dev',
-// 		'username' => 'jtcc_dev',
-// 		'password' => 'VdleL7N_krUS',
-// 		'dbprefix' => 'exp_',
-// 		'char_set' => 'utf8mb4',
-// 		'dbcollat' => 'utf8mb4_unicode_ci',
-// 		'port'     => ''
-// 	),
-// );
+$config['show_profiler']        = 'n'; # y/n
+$config['template_debugging']   = 'n'; # y/n
+$config['save_tmpl_files']      = "y";  # y/n
+$config['debug']                = "1"; # 0: no errors shown. 1: Errors shown to Super Admins. 2: Errors shown to everyone.
+$config['enable_sql_caching']   = 'n'; # Cache Dynamic Channel Queries?
+$config['email_debug']          = 'n'; # y/n
+
 
 switch($_SERVER['HTTP_HOST']) {
 
@@ -83,8 +67,17 @@ switch($_SERVER['HTTP_HOST']) {
                 );
     break;
 }
-$config['database'] = array(
-	'expressionengine' => $dbConnection
+
+$config['database'] = array (
+  'expressionengine' => $dbConnection
 );
 
-// EOF
+$config['enable_devlog_alerts']      = 'n';
+$config['app_version'] = '5.3.2';
+$config['encryption_key'] = '78e307bb286224bd336e71417c800997eb9612a5';
+$config['session_crypt_key'] = 'f4b860726dbfee5bb8dddda232142261abbb8550';
+$config['share_analytics']           = 'y';
+$config['show_ee_news']              = 'y';
+$config['multiple_sites_enabled']    = 'n';
+
+// END EE config items
